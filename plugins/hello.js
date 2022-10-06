fd5e90a531628715c7b615cd3d5f7a362107cd64
Reza11Ofc <113843050+Reza11Ofc@users.noreply.github.com>
2022-10-07 03:19:34 +0700

    Update hello.js

--- plugins/hello.js
+++ plugins/hello.js
@@ -6,10 +6,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
 conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaUrl: "https://chat.whatsapp.com/Htbopc53nFD3puFb3LfMe1",
     mediaType: 2,
-    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
-    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    description: "https://chat.whatsapp.com/Htbopc53nFD3puFb3LfMe1", 
+    title: 'EzaBot-MD',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig  }}})
